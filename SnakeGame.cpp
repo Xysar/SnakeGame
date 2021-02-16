0a3ef98affc035d83e8698b3d2e5f1308a905b2d
Xysar <Cesarcamacho760@gmail.com>
2021-02-16 16:26:01 -0500

    Visuals implemented

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -2,10 +2,101 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <conio.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+bool gameOver;
+const int width = 30;
+const int height = 16;
+int x, y, fruitX, fruitY, score;
+
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+
+eDirection dir;
+
+void setup() {
+	gameOver = false;
+	dir = STOP;
+	x = width / 2;
+	y = height / 2;
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+}
+
+void draw() {
+	system("cls");
+	for (int i = 0; i < width; i++)
+		cout << "#";
+	cout << endl;
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			if (j == 0 || j == (width - 1))
+				cout << "#";
+			else if (j == x && i == y)
+				cout << "0";
+			else if (j == fruitX && i == fruitY)
+				cout << "@";
+			else
+				cout << " ";
+
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < width; i++)
+		cout << "#";
+	cout << endl;
+}
+
+void input() {
+	if (_kbhit()) {
+		switch (_getch()) {
+		case 'a':
+			dir = LEFT;
+			break;
+		case'd':
+			dir = RIGHT;
+			break;
+		case'w':
+			dir = UP;
+			break;
+		case 's':
+			dir = DOWN;
+			break;
+		case 'x':
+			gameOver = true;
+		}
+	}
+}
+
+void logic() {
+	switch (dir) {
+	case LEFT:
+		x--;
+		break;
+	case RIGHT:
+		x++;
+		break;
+	case UP:
+		y--;
+		break;
+	case DOWN:
+		y++;
+		break;
+
+
+	}
+}
+
+int main() {
+	setup();
+	while (!gameOver) {
+		draw();
+		input();
+		logic();
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
