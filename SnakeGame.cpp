edbb53c56b3a38c25cbc5fb73f53d6eb7666e6b7
Xysar <Cesarcamacho760@gmail.com>
2021-02-16 17:26:11 -0500

    Finished Game

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -4,12 +4,15 @@
 #include <iostream>
 #include <vector>
 #include <conio.h>
+#include <Windows.h>
 
 using namespace std;
 bool gameOver;
 const int width = 30;
 const int height = 16;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
 
 enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
 
@@ -20,8 +23,9 @@ void setup() {
 	dir = STOP;
 	x = width / 2;
 	y = height / 2;
-	fruitX = rand() % width;
-	fruitY = rand() % height;
+	fruitX = (rand() % (width - 1)) + 1;
+	fruitY = (rand() % (height - 1)) + 1;
+	score = 0;
 }
 
 void draw() {
@@ -39,7 +43,19 @@ void draw() {
 			else if (j == fruitX && i == fruitY)
 				cout << "@";
 			else
+			{
+				bool print = false;
+				for (int k = 0; k < nTail; k++) {
+					
+					if (tailX[k] == j && tailY[k] == i) {
+						cout << "o";
+						print = true;
+					}
+				}
+
+				if(!print)
 				cout << " ";
+			}
 
 		}
 		cout << endl;
@@ -48,6 +64,7 @@ void draw() {
 	for (int i = 0; i < width; i++)
 		cout << "#";
 	cout << endl;
+	cout << "Score: " << score << endl;
 }
 
 void input() {
@@ -72,6 +89,20 @@ void input() {
 }
 
 void logic() {
+	int prevX = tailX[0];
+	int prevY = tailY[0];
+	int prev2X, prev2Y;
+	tailX[0] = x;
+	tailY[0] = y;
+	for (int i = 1; i < nTail; i++) {
+		prev2X = tailX[i];
+		prev2Y = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX = prev2X;
+		prevY = prev2Y;
+	}
+
 	switch (dir) {
 	case LEFT:
 		x--;
@@ -85,8 +116,20 @@ void logic() {
 	case DOWN:
 		y++;
 		break;
-
-
+	default:
+		break;
+	}
+	if (x == width || x == 0 || y == height || y == 0)
+		gameOver = true;
+	for(int i = 0; i < nTail; i++) {
+		if (tailX[i] == x && tailY[i] == y)
+		gameOver = true;
+	}
+	if (x == fruitX && y == fruitY) {
+		score++;
+		fruitX = (rand() % (width - 1)) + 1;
+		fruitY = (rand() % (height - 1)) + 1;
+		nTail++;
 	}
 }
 
@@ -96,6 +139,10 @@ int main() {
 		draw();
 		input();
 		logic();
+		if (dir == UP || dir == DOWN)
+			Sleep(40);
+		else
+			Sleep(20);
 	}
 }
 
